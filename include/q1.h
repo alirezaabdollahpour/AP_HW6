0d6e77d102e6113a06751f70b2a51c3a624db8ee
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-31 10:07:36 +0430

    q1 done!

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,34 @@
 #ifndef Q1_H
 #define Q1_H
 
-#endif //Q1_H
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace q1 {
+
+template <typename T, typename Func>
+double moshtagh(T point, Func func)
+{
+    double eps { 0.001 };
+    double moshataghat { (func(point + eps) - func(point - eps)) / (2.0 * eps) };
+    return moshataghat;
+}
+
+template <typename T, typename Func>
+double gradient_descent(T ivalue, double step, Func func = Func {})
+{
+    double lr { step };
+    T iv { ivalue };
+    double taghirat { lr * moshtagh(iv, func) };
+    while (std::abs(taghirat) >= 0.00001) {
+        taghirat = lr * moshtagh(iv, func);
+        iv = iv - taghirat;
+    }
+    return iv;
+}
+}
+
+#endif
