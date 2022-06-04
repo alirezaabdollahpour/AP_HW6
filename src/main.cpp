9752fb3151514d5de17b6e4a4f6cc6c310139a2c
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-06-04 08:37:35 +0430

    main.cpp done

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-#include "q1.h"
-#include "q2.h"
-#include "q3.h"
-#include "q4.h"
+// #include "q1.h"
+// #include "q2.h"
+// // #include "q3.h"
+// // #include "q4.h"
 
 int main(int argc, char** argv)
 {
-    if (true) // make false to run unit-tests
+    if (false) // make false to run unit-tests
     {
-        // debug section
-        std::cout << "hello to HW6" << std::endl;
 
     } else {
+        std::cout << "running your unit_test" << std::endl;
+
         ::testing::InitGoogleTest(&argc, argv);
         std::cout << "RUNNING TESTS ..." << std::endl;
         int ret { RUN_ALL_TESTS() };
